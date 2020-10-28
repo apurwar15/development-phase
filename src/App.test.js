f1993d004a6e634da3070d873c6df7b06be0ff14
ArpitPurwar <apurwar@perforce.com>
2020-10-28 13:51:36 +0530

    test

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,7 @@ import App from './App';
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/Arpit Purwar/i);
+
   expect(linkElement).toBeInTheDocument();
 });
 
